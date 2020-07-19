855019f834d776b3c0867b688be5b9adf18256f5
André Moraes <amancioandre@gmail.com>
2020-07-19 15:51:22 -0300

    feat: add remainingTime utils

---
+++ src/utils/index.js
@@ -0,0 +1,18 @@
+import moment from 'moment'
+
+export const remainingTime = (targetDate, currentDate) => {
+    const remainingDays = moment(targetDate).diff(currentDate, 'days')
+    const remainingHours = moment(targetDate)
+    .subtract(remainingDays, 'days')
+    .diff(currentDate, 'hours')
+    const remainingMinutes = moment(targetDate)
+    .subtract(remainingDays, 'days')
+    .subtract(remainingHours, 'hours')
+    .diff(currentDate, 'minutes')
+    const remainingSeconds = moment(targetDate)
+    .subtract(remainingDays, 'days')
+    .subtract(remainingHours, 'hours')
+    .subtract(remainingMinutes, 'minutes')
+    .diff(currentDate, 'seconds')
+  return { remainingDays, remainingHours, remainingMinutes, remainingSeconds };
+}
